36aef4440cd3e6290485160453d68389d87af38f
larsenkw <kylew.larsen@gmail.com>
2020-07-03 00:23:13 -0600

    Added calibration and linear and angular velocities

--- src/torso_control.py
+++ src/torso_control.py
@@ -24,22 +24,32 @@ class TorsoControl:
 
         # Parameters
         self.calibration_time = rospy.get_param('~calibration_time', 3.0) # sec
-        self.deadband_radius = rospy.get_param('~deadband_radius', 0.2)
+        self.deadband_x = rospy.get_param('~deadband_x', 0.2)
+        self.deadband_y = rospy.get_param('~deadband_y', 0.2)
+        self.deadband_angle = rospy.get_param('~deadband_angle', math.pi/8)
         self.fixed_frame = rospy.get_param('~fixed_frame', 'camera_link') # name of the parent frame to 'joystick' frame
+        self.joystick_frame = rospy.get_param('~joystick_frame', 'joystick')
         self.max_user_tilt_angle = math.pi/4
         self.x_max_length = math.cos(math.pi/2 - self.max_user_tilt_angle)
         self.y_max_length = math.cos(math.pi/2 - self.max_user_tilt_angle)
+
+        # Check to ensure the deadband does not exceed the max values
+        if (self.x_max_length <= self.deadband_x):
+            rospy.logerr("[{0}]: The deadband radius is larger than the maximum for the X dimension. Please decrease the deadband for x or increase the 'x_max_length' variable in the {0} node.".format(rospy.get_name()))
+            rospy.signal_shutdown("[{0}]: Deadband radius error.".format(rospy.get_name()))
+        if (self.y_max_length <= self.deadband_y):
+            rospy.logerr("[{0}]: The deadband radius is larger than the maximum for the Y dimension. Please decrease the deadband radius or increase the 'y_max_length' variable in the {0} node.".format(rospy.get_name()))
+            rospy.signal_shutdown("[{0}]: Deadband radius error.".format(rospy.get_name()))
+
         self.velocity_x_max = 2 # m/s
         self.velocity_y_max = 2 # m/s
         self.velocity_angular_max = math.pi/4 # rad/s
+        self.velocity_angular_scale = 1
 
         # Variables
         self.joystick_x = 0
         self.joystick_y = 0
-        self.l_shoulder_x = 0
-        self.l_shoulder_y = 0
-        self.r_shoulder_x = 0
-        self.r_shoulder_y = 0
+        # Rotation from the joystick frame to the fixed frame
         self.joystick_quaternion = [0, 0, 0, 1]
 
         # TF Listener/Broadcaster
@@ -66,30 +76,164 @@ class TorsoControl:
         while not rospy.is_shutdown():
             # Calculate velocities
             # If there are any errors, velocities are set to 0
-            if not self.calculateLinearVelocity():
+            base_link, l_shoulder, r_shoulder = self.getTransforms()
+            if base_link is None:
                 self.cmd_vel_msg.linear.x = 0
                 self.cmd_vel_msg.linear.y = 0
-            if not self.calculateAngularVelocity():
                 self.cmd_vel_msg.angular.z = 0
+            else:
+                self.cmd_vel_msg.linear.x, self.cmd_vel_msg.linear.y = self.calculateLinearVelocity(base_link)
+                self.cmd_vel_msg.angular.z = self.calculateAngularVelocity(l_shoulder, r_shoulder)
 
             # Publish velocity
             self.cmd_vel_pub.publish(self.cmd_vel_msg)
 
             self.rate.sleep()
 
-    def calculateLinearVelocity(self):
+    def getTransforms(self):
+        '''
+        Looks up the transform for the base_link in the fixed frame. Uses
+        base_link's position as the joystick frame's position and broadcasts the
+        joystick transform. Finds the shoulder transforms in the joystick frame.
+        Returns the orientation for the base_link and the positions for the left
+        and right shoulders. These values are subsequently used to calculate the
+        linear and angular velocities. If there are any errors, "base_link" is
+        returned as "None".
+
+        Args
+        ----
+        None
+
+        Returns
+        -------
+        base_link:  4 element list, orientation of base_link in joystick frame
+                    as quaternion [x, y, z, w].
+        l_shoulder: 3 element list, position of LShoulder_K in joystick frame.
+        r_shoulder: 3 element list, position of RShoulder_K in joystick frame.
+        '''
+        # Lookup base_link
+        base_link_position = [0, 0, 0]
+        base_link_rotation = [0, 0, 0, 1]
+        try:
+            self.tfListener.waitForTransform(self.fixed_frame, 'base_link_K', rospy.Time(), rospy.Duration.from_sec(1.0))
+            base_link_position, base_link_rotation = self.tfListener.lookupTransform(self.fixed_frame, 'base_link_K', rospy.Time())
+        except tf.Exception as err:
+            rospy.loginfo("[{0}]: {1}".format(rospy.get_name(), err))
+            return None, None, None
+
+        # Publish joystick frame
+        self.tfBroadcaster.sendTransform(base_link_position, self.joystick_quaternion, rospy.Time.now(), self.joystick_frame, self.fixed_frame)
+
+        # Calculate base_link orientation in joystick frame
+        # b = base_link, f = fixed_frame, j = joystick
+        # f_R_b = rotation from base_link to fixed frame
+        # f_R_j = rotation from joystick to fixed frame
+        # j_R_b = (f_R_j))^-1 * f_R_b
+        q_joystick_inverse = tf.transformations.quaternion_inverse(self.joystick_quaternion)
+        base_link = tf.transformations.quaternion_multiply(q_joystick_inverse, base_link_rotation)
+
+        # Lookup shoulders in joystick frame
+        l_shoulder = [0, 0, 0]
+        r_shoulder = [0, 0, 0]
+        try:
+            self.tfListener.waitForTransform(self.joystick_frame, 'LShoulder_K', rospy.Time(), rospy.Duration.from_sec(1.0))
+            l_shoulder, _ = self.tfListener.lookupTransform(self.joystick_frame, 'LShoulder_K', rospy.Time())
+            self.tfListener.waitForTransform(self.joystick_frame, 'RShoulder_K', rospy.Time(), rospy.Duration.from_sec(1.0))
+            r_shoulder, _ = self.tfListener.lookupTransform(self.joystick_frame, 'RShoulder_K', rospy.Time())
+        except tf.Exception as err:
+            rospy.loginfo("[{0}]: {1}".format(rospy.get_name(), err))
+            return None, None, None
+
+        return base_link, l_shoulder, r_shoulder
+
+    def calculateLinearVelocity(self, base_link_quaternion):
         '''
         Calculate the X and Y linear velocities of Pepper's base using the
         operator's torso as a joystick.
-        '''
-        return False
 
-    def calculateAngularVelocity(self):
+        Args
+        ----
+        base_link_quaternion: 4 element list, base_link orientation in joystick
+                              frame.
+
+        Returns
+        -------
+        velocity_x: linear velocity in the x direction
+        velocity_y: linear velocity in the y direction
+        '''
+        # Initialize velocity
+        velocity_x = 0.0
+        velocity_y = 0.0
+
+        # Convert the quaternion into a rotation matrix
+        j_R_b = tf.transformations.quaternion_matrix(base_link_quaternion)
+
+        # Extract the X and Y values of the Z axis
+        self.joystick_x = j_R_b[0,2]
+        self.joystick_y = j_R_b[1,2]
+
+        # Check deadband
+        if (abs(self.joystick_x) > self.deadband_x):
+            # Saturate the joystick value
+            self.joystick_x = min(self.joystick_x, self.x_max_length)
+            self.joystick_x = max(self.joystick_x, -self.x_max_length)
+
+            # Shift the joystick origin to the deadband position
+            self.joystick_x = self.joystick_x - math.copysign(self.deadband_x, self.joystick_x)
+
+            # Map to a range from 0 to 1
+            self.joystick_x = self.joystick_x / (self.x_max_length - self.deadband_x)
+
+            # Apply scaling
+            velocity_x = self.velocity_x_max * self.joystick_x
+
+        if (abs(self.joystick_y) > self.deadband_y):
+            # Saturate the joystick value
+            self.joystick_y = min(self.joystick_y, self.y_max_length)
+            self.joystick_y = max(self.joystick_y, -self.y_max_length)
+
+            # Shift the joystick origin to the deadband position
+            self.joystick_y = self.joystick_y - math.copysign(self.deadband_y, self.joystick_y)
+
+            # Map to a range from 0 to 1
+            self.joystick_y = self.joystick_y / (self.y_max_length - self.deadband_y)
+
+            # Apply scaling
+            velocity_y = self.velocity_y_max * self.joystick_y
+
+        return velocity_x, velocity_y
+
+    def calculateAngularVelocity(self, l_shoulder_position, r_shoulder_position):
         '''
         Calculate the angular velocity of Pepper's base using the operator's
         shoulder angle.
+
+        Args
+        ----
+        l_shoulder_position: 3 element list, (x,y,z) position of 'LShoulder_K'
+        r_shoulder_position: 3 element list, (x,y,z) position of 'RSHoulder_K'
+
+        Returns
+        -------
+        velocity_angular: angular velocity of the base
         '''
-        return False
+        velocity_angular = 0.0
+
+        # Translate shoulder projecting onto joystick X-Y plane such that right
+        # shoulder is on the origin. Find the angle of the line passing between
+        # the shoulders with respect to the Y axis
+        l_shoulder_x = l_shoulder_position[0] - r_shoulder_position[0]
+        l_shoulder_y = l_shoulder_position[1] - r_shoulder_position[1]
+
+        shoulder_theta = math.atan2(l_shoulder_y, l_shoulder_x) - math.pi/2
+
+        # Apply deadband
+        if (abs(shoulder_theta) > self.deadband_angle):
+            velocity_angular = self.velocity_angular_scale*(shoulder_theta - math.copysign(self.deadband_angle, shoulder_theta))
+            velocity_angular = min(velocity_angular, self.velocity_angular_max)
+            velocity_angular = max(velocity_angular, -self.velocity_angular_max)
+
+        return velocity_angular
 
     def calibrateJoystickPose(self, msg):
         '''
@@ -98,8 +242,32 @@ class TorsoControl:
         frame orientation and used to find the velocities based on the deviation
         of the torso with respect to this frame.
         '''
-        return False
+        rospy.loginfo('[{0}]: Calibrating for {1} seconds.'.format(rospy.get_name(), self.calibration_time))
+        calibration_start = rospy.get_time()
 
+        try:
+            # Obtain the first quaternion
+            self.tfListener.waitForTransform(self.fixed_frame, 'base_link_K', rospy.Time(), rospy.Duration.from_sec(1.0))
+            position, self.joystick_quaternion = self.tfListener.lookupTransform(self.fixed_frame, 'base_link_K', rospy.Time())
+            count = 1
+
+            # Iterate until time is reached
+            while (rospy.get_time() - calibration_start) < self.calibration_time:
+                # Read next torso orientation
+                self.tfListener.waitForTransform(self.fixed_frame, 'base_link_K', rospy.Time(), rospy.Duration.from_sec(1.0))
+                position, new_quaternion = self.tfListener.lookupTransform(self.fixed_frame, 'base_link_K', rospy.Time())
+                count += 1
+
+                # Average orientation
+                self.joystick_quaternion = tf.transformations.quaternion_slerp(self.joystick_quaternion, new_quaternion, 1.0/count)
+
+        except tf.Exception as err:
+            rospy.loginfo('[{0}]: {1}'.format(rospy.get_name(), err))
+            return False
+
+        rospy.loginfo("[{0}]: Calibration complete. Using joystick frame quaternion [{1}, {2}, {3}, {4}]".format(rospy.get_name(), self.joystick_quaternion[0], self.joystick_quaternion[1], self.joystick_quaternion[2], self.joystick_quaternion[3]))
+
+        return True
 
     def shutdown(self):
         '''

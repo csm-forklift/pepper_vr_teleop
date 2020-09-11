2095febc58be6cd65a056532ff33e976720e9f66
larsenkw <kylew.larsen@gmail.com>
2020-09-11 02:25:19 -0600

    Added docstrings to module and classes.

--- src/libtcp.py
+++ src/libtcp.py
@@ -1,7 +1,16 @@
-"""
-Classes used for TCP communication. They handle checking the state and
-processing the data. The client and server act in the following ways:
+""" Module containing classes used to create TCP connections using Python.
 
+The TCPClient class is used by the 'image_stream' node to communicate with
+Pepper's tablet to send images from a ROS topic publishing a video stream.
+Pepper has an app developed in Android Studio, called "Pepper Image Stream",
+which waits for a TCP connection, then proceeds to display the images it
+receives in fullscreen on the tablet. The message flow of the client is
+specifically designed to communicate with the server in a particular sequence. A
+server class is provided in this module as a reference and for testing, but it
+is not used in the node. The Python ROS node only acts as a client. The Android
+app acts as the server.
+
+Here is the general message passing procedure:
 Client:
 1) Connect to server
 2) Generate request
@@ -19,6 +28,22 @@ Server:
 6) Generate response
 7) Send response
 8) Go to 3
+
+Classes
+-------
+TCP : object
+    A set of functions for communicating using TCP.
+TCPServer : TCP
+    Creates a TCP server socket using Python's socket module.
+TCPClient : TCP
+    Creates a TCP client connection using Python's socket module.
+
+Exceptions
+----------
+TCPError : Exception
+    Base class for TCP exceptions.
+ConnectionClosedError : TCPError
+    Exception raised when a connection is lost.
 """
 
 import socket
@@ -31,6 +56,92 @@ import cv2
 from threading import Lock
 
 class TCP(object):
+    """ A set of functions for communicating using TCP.
+
+    Contains the basic functions needed to send and receive messages over a TCP
+    socket in Python. The functions provided assume a message with the following
+    structure:
+    Protocol Header
+        2 bytes, unsigned int, big-endian
+        The size of the JSON header
+    JSON Header
+        Contains information about the content, including its size in bytes.
+    Content
+        The raw bytes of the content which must be processed into whatever
+        pre-determined format.
+
+    The server and client subclasses must implement the 'create_message' and
+    'process_content' messages based on the format of the Request and Response
+    messages.
+
+    Attributes
+    ----------
+    socket : socket.socket
+        The local socket object.
+    local_address
+        The local IP address and port combined
+    local_host
+        The local IP address as a string.
+    local_port
+        The local port number.
+    remote_address
+        The remote IP address and port combined.
+    remote_host
+        The remote IP address as a string.
+    remote_port
+        The remote port number.
+    receive_size
+        The max number of bytes to read at one time.
+    _recv_buffer
+        The receive buffer array.
+    _send_buffer
+        The send buffer array.
+    _json_header_len
+        The size of the JSON header.
+    json_header
+        The JSON header contents.
+    content
+        The content from the message being received.
+
+    Methods
+    -------
+    read()
+        Reads raw data bytes from the socket.
+    write() -> bool
+        Sends data in buffer. Returns whether message was fully sent.
+    _json_encode(obj, encoding) -> byte array
+        Encode dictionary into JSON byte array.
+    _json_decode(json_bytes, encoding) -> dict
+        Decode a dictionary from a JSON byte array.
+    _int_to_bytes(size) -> byte array
+        Convert an Int into two raw bytes, big-endian.
+    _bytes_to_int(bytes) -> int
+        Convert two bytes into an unsigned int.
+    process_protocol_header()
+        Creates the protocol header bytes.
+    process_json_header()
+        Creates the JSON header bytes.
+    process_message()
+        Converts the receive buffer bytes into data.
+    read_message()
+        Reads from the socket until a full message is received.
+    write_message()
+        Creates message and sends over socket.
+    close()
+        Closes the socket.
+
+    Abstract Methods
+    ----------------
+    process_content()
+    create_message()
+
+    Parameters
+    ----------
+    sock : socket.socket
+        The local socket object.
+    receive_size : int
+        The max number of bytes to read at one time.
+    """
     def __init__(self, sock=None, receive_size=4096):
         self.socket = sock
         self.local_address = self.socket.getsockname()
@@ -108,6 +219,10 @@ class TCP(object):
             The data bytes to be converted into a Python variables.
         encoding : str
             The encoding type for the JSON text. Most common is 'utf-8'.
+
+        Returns
+        -------
+        Dictionary representing JSON object.
         """
         return json.loads(json_bytes.decode(encoding))
 
@@ -118,6 +233,10 @@ class TCP(object):
         ----------
         size : int
             The size of the JSON header array.
+
+        Returns
+        -------
+        Big-endian byte array
         """
         return struct.pack('>H', size)
 
@@ -128,6 +247,10 @@ class TCP(object):
         ----------
         bytes : byte array
             The pair of bytes to be converted into an Int (JSON header size).
+
+        Returns
+        -------
+        int
         """
         return struct.unpack('>H', bytes)[0]
 
@@ -161,17 +284,20 @@ class TCP(object):
             self.process_content()
 
     def read_message(self):
+        """ Reads from the socket until a full message is received. """
         while self.content is None:
             self.read()
             self.process_message()
 
     def write_message(self):
+        """ Creates message and sends over socket. """
         self.create_message()
         successfully_sent = False
         while not successfully_sent:
             successfully_sent = self.write()
 
     def close(self):
+        """ Closes the socket. """
         print('Closing connection:\n\t {0}:{1} --> {2}:{3}'.format(self.local_host, self.local_port, self.remote_host, self.remote_port))
         self.socket.close()
 
@@ -185,6 +311,54 @@ class TCP(object):
         raise NotImplementedError()
 
 class TCPServer(TCP):
+    """ Creates a TCP server socket using Python's socket module.
+
+    This class contains the variables and functions required to make a TCP
+    server socket available for accepting clients. A specific message passing
+    sequence has been coded into the functions. See the module description to
+    see the flow of communication.
+
+    Attributes
+    ----------
+    encoding : str
+        The JSON text encoding type. Should be 'utf-8' unless the code has been
+        changed to handle other formats.
+    response : bool
+        The Response message content.
+    server_host : str
+        The server IP address
+    server_port : int
+        The server port.
+    server_address : tuple
+        The server IP address and port combined.
+    server_socket : socket.socket
+        The server socket object.
+    connection : socket.socket
+        The client socket object.
+    connection_address : tuple
+        The client IP address and port combined.
+
+    Methods
+    -------
+    create_message()
+        Generate the Response message bytes.
+    process_content()
+        Process the Request content.
+    process_loop()
+        Loops through the message passing process for each request/response
+        cycle.
+    reset()
+        Resets the variables and waits for a re-connection.
+    close_server()
+        Close the server socket.
+
+    Parameters
+    ----------
+    host : str
+        The IP address of the server socket.
+    port : int
+        The port of the server socket.
+    """
     def __init__(self, host='127.0.0.1', port=65432):
         #======================================================================#
         # Create Socket
@@ -276,7 +450,7 @@ class TCPServer(TCP):
     #==========================================================================#
     def process_loop(self):
         """ Loops through the message passing process for each request/response
-        cycle
+        cycle.
         """
         while True:
             try:
@@ -311,34 +485,78 @@ class TCPServer(TCP):
         self.content = None
 
     def close_server(self):
-        # Close the server socket
+        """ Close the server socket. """
         print('Closing server at {0}:{1}'.format(self.server_host, self.server_port))
         self.server_socket.close()
 
 class TCPClient(TCP):
+    """ Creates a TCP client connection using Python's socket module.
+
+    This class contains the variables and functions required to make a TCP
+    client connection to a server. A specific message passing sequence has been
+    coded into the functions. See the module description to see the flow of
+    communication.
+
+    Attributes
+    ----------
+    encoding : str
+        The JSON text encoding type. Should be 'utf-8' unless the code has been
+        changed to handle other formats.
+    available_image_types : list of str
+        The possible image extensions that can be used when compressing and
+        streaming to the Android app.
+    image_type : str
+        The image extension to use when streaming the image to the app.
+    compression_value : int
+        The value relating to how much the image should be compressed. PNG has a
+        range from 0 (least compressed) to 9 (most compressed). JPEG has a range
+        from 100 percent quality (least compressed) to 0 percent quality (most
+        compressed).
+    socket : socket.socket
+        The client socket that connects to the server.
+    new_image : bool
+        Indicates whether a new images has been received since the last
+        transfer.
+    image_lock : thread.Lock
+        A lock used to inhibit another process thread from editing the image
+        variable while it is either being stored or sent.
+
+    Methods
+    -------
+    create_message()
+        Generate the Request message bytes.
+    process_content()
+        Process the Response content.
+    spin_once() -> bool
+        Writes a message then waits for the response. Returns whether
+        successful.
+    set_new_image(image)
+        Updates the 'image' variable to be sent to the app.
+    get_available_image_types() -> list of str
+        Returns the list of available image extensions.
+
+    Parameters
+    ----------
+    remote_host : str
+        The IP address of the server socket.
+    remote_port : int
+        The port of the server socket.
+    image_type : {'bmp', 'png', 'jpeg'}
+        The image type to use when sending the image to the server. 'bmp'
+        has no compression. 'png' has lossless compression. 'jpeg' has lossy
+        compression but can be reduced to a smaller file size if desired.
+        The default type is 'jpeg' since it compresses the best for image
+        transfer.
+    compression : int
+        Compression percentage from 0 to 100. The 'png' format has
+        compression values as integers from 0 to 9. In this case the
+        compression will be mapped from 0 to 100 to the range 0 to 9 (9
+        being the most compression). The 'jpeg' format compresses on a 0 to
+        100 scale based on quality where 0 is the worse quality and 100 is
+        the best. In this case the compression value will be inverted by
+        being mapped from 0 to 100 to the range 100 to 0.
+    """
     def __init__(self, remote_host='127.0.0.1', remote_port=65432, image_type='jpeg', compression=50):
-        """
-        Parameters
-        ----------
-        remote_host : str
-            The IP address of the server socket.
-        remote_port : int
-            The port of the server socket.
-        image_type : {'bmp', 'png', 'jpeg'}
-            The image type to use when sending the image to the server. 'bmp'
-            has no compression. 'png' has lossless compression. 'jpeg' has lossy
-            compression but can be reduced to a smaller file size if desired.
-            The default type is 'jpeg' since it compresses the best for image
-            transfer.
-        compression : int
-            Compression percentage from 0 to 100. The 'png' format has
-            compression values as integers from 0 to 9. In this case the
-            compression will be mapped from 0 to 100 to the range 0 to 9 (9
-            being the most compression). The 'jpeg' format compresses on a 0 to
-            100 scale based on quality where 0 is the worse quality and 100 is
-            the best. In this case the compression value will be inverted by
-            being mapped from 0 to 100 to the range 100 to 0.
-        """
         #======================================================================#
         # Create Socket
         #======================================================================#
@@ -375,7 +593,12 @@ class TCPClient(TCP):
     # Define Request Message
     #==========================================================================#
     def create_message(self):
-        """ Generate the Request message bytes. """
+        """ Generate the Request message bytes.
+
+        Converts the message data into a byte array, generates the message
+        headers and converts them into byte arrays, thens stores the bytes into
+        a buffer.
+        """
         # Convert image to appropriate encoding
         if (self.image_type == 'bmp'):
             retval, content_bytes = cv2.imencode('.bmp', self.image)
@@ -426,6 +649,7 @@ class TCPClient(TCP):
     # Define State Flow
     #==========================================================================#
     def spin_once(self):
+        """ Process loop: writes a message, waits for response. """
         try:
             # Send out request
             if (self.new_image):

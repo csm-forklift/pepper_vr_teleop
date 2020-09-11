ac8655d8c3e96f12337171e52ad5ab5269ec5832
larsenkw <kylew.larsen@gmail.com>
2020-09-11 02:36:25 -0600

    Added docstrings to code with ROS node description.

--- src/image_stream.py
+++ src/image_stream.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
-"""
+""" Sends a ROS image stream to the PepperImageStream app.
+
 ROS node that subscribes to a camera feed and then sends the images to an
 Android application through TCP. The Android app is called "Pepper Image
 Stream".
+
+ROS Node Description
+====================
+Parameters
+----------
+~server_ip : str, default: '127.0.0.1'
+    The TCP server IP address as a string.
+~server_port : int, default: 65432
+    The TCP server port number.
+~image_type : str, default: 'jpeg'
+    The image file extension to use when sending the file to the app.
+~compression : int, default: 50
+    The compression percentage from 0 to 100.
+~image_topic : str, default: '/camera/rgb/image_color'
+    The name of the topic publishing an image.
+
+Published Topics
+----------------
+None
+
+Subscribed Topics
+-----------------
+<~image_topic> : sensor_msgs/Image
+    The incoming images to be sent to the Android app.
 """
 
+# Python
 from libtcp import TCPClient
 
+# ROS
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -14,7 +41,7 @@ from sensor_msgs.msg import Image
 class ImageStream(object):
     def __init__(self):
         #======================================================================#
-        # Initialize ROS
+        # ROS Setup
         #======================================================================#
         rospy.init_node('image_stream')
         self.frequency = 30
@@ -47,14 +74,22 @@ class ImageStream(object):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(self.image_topic, Image, self.imageCallback, queue_size=1)
 
+    #==========================================================================#
+    # Main Process
+    #==========================================================================#
     def spin(self):
+        """ Process loop: spin client. """
         while not rospy.is_shutdown():
             success = self.client.spin_once()
             if not success:
                 break
             self.rate.sleep()
 
+    #==========================================================================#
+    # Callback Functions
+    #==========================================================================#
     def imageCallback(self, msg):
+        """ Read the new image and send to client. """
         # Convert ROS Image msg to opencv format
         image = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
 
